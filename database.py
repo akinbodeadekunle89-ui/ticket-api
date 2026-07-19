4b7f854d58958fff1052ef3135995237dcd1aaa2
akinbodeadekunle89-ui <akinbodeadekunle89@gmail.com>
2026-07-19 04:23:15 -0700

    Add database.py to repository

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, SQLModel
 
 # Replace with your actual database URL (e.g., sqlite:///./tickets.db)
 sqlite_url = "sqlite:///./tickets.db"
